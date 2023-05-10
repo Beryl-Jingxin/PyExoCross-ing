badcc0f77a6781a58ccddcc12d0498595542a25f
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2023-05-10 18:42:54 +0100

    Update

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -9,9 +9,20 @@ window.MathJax = {
       ignoreHtmlClass: ".*|",
       processHtmlClass: "arithmatex"
     }
-  };
+};
   
-  document$.subscribe(() => {
+document$.subscribe(() => {
     MathJax.typesetPromise()
-  })
-  
+})
+
+MathJax.Hub.Config({
+    "tex2jax": { 
+        inlineMath: [["$","$"], ["\\(","\\)"]],
+        displayMath: [["$$","$$"], ["\\[", "\\]"]],
+    }
+});
+MathJax.Hub.Config({
+    config: ["MMLorHTML.js"],
+    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+    extensions: ["MathMenu.js", "MathZoom.js"]
+});
